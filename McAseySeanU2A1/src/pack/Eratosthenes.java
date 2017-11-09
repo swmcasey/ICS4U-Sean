ba0c202bced7c79590adefb670ffa75991cff443
Sean McAsey <s207681@ocsb.local>
2017-11-09 12:07:08 -0500

    2017-11-09 school

--- McAseySeanU2A1/src/pack/Eratosthenes.java
+++ McAseySeanU2A1/src/pack/Eratosthenes.java
@@ -4,28 +4,37 @@ import java.util.ArrayList;
 
 public class Eratosthenes {
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> primes = new ArrayList<Integer>(); // declare an ArrayList to put primes in
-		for (int i = 1; i <= 1000; i++) {
-			primes.add(i);
-		}
+		int x = 100000000; // upper bound for primes
+		int total = 0;
+		Boolean[] notPrime = new Boolean[x]; // declare an array to put primes
+												// in
 
-		for (int i = 1; i < primes.size(); i++) { // for the lowest prime not yet removed or tested,
-			
-			for (int c = 2; c <= 1000 / primes.get(i); c++) { // multiply it by all coefficients that won't put it above half the max value and aren't 1,
-			
-				for (int n = 1; n < primes.size(); n++) { // and test it against every prime that's still in the list
-				
-					if (primes.get(n) == c * primes.get(i)) { // if the prime i multiplied by this coefficient is equal to this number, it is not prime
-					
-						primes.remove(n); // so it is removed
-						break; // and the coefficient is no longer tested since its value was matched
-					}
+		for (int i = 0; i < notPrime.length; i++) {
+			notPrime[i] = false;
+		}
+		notPrime[0] = true;
+
+		for (int i = 2; i <= x / 2; i++) {
+			if (!notPrime[i - 1]) {
+				for (int o = 2; o <= x / i; o++) {
+					notPrime[i * o - 1] = true;
 				}
 			}
 		}
-		for (int i = 0; i < primes.size(); i++) {
-			System.out.println(primes.get(i));
+		String output = "";
+		int count = 0;
+		for (int i = 0; i < x; i++) {
+			if (!notPrime[i]) {
+				count++;
+				total++;
+				if (total < 30) {
+					output += i + 1 + " ";
+				} else {
+					total = 0;
+					output += i + 1 + "\n";
+					System.out.println(output);
+				}
+			}
 		}
 	}
 }
