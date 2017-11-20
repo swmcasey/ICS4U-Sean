5bde80f3b467740d6d587459583b54dd8bde47bd
Sean McAsey <s207681@ocsb.local>
2017-11-20 12:28:54 -0500

    2017-11-20 school

--- McAseySeanU3A1/src/a1/Car.java
+++ McAseySeanU3A1/src/a1/Car.java
@@ -1,51 +1,77 @@
 package a1;
 
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Car {
 	  //variables for characteristics
 
-	  private String carModel, carMake, carPrice;
-	  private int carYear;
+	  private String carModel, carMake, carColour;
+	  private int carYear, carPrice;
+	  private boolean isUsed;
 
 	  //constructors
 	  public Car() {
 	    carModel = "Ford";
 	    carMake = "Model T";
+	    carColour = "Black";
 	    carYear = 1908;
-	    carPrice = "$825";
+	    carPrice = 825;
+	    isUsed = false;
 	  }
 
-	  public Car(String model, String make, int agg, int hung) {
+	  public Car(String make, String model, String colour, int year, int price, boolean used) {
 	    carModel = model;
 	    carMake = make;
-	    carYear = agg;
-	    carPrice = "$"+hung;
+	    carColour = colour;
+	    carYear = year;
+	    carPrice = price;
+	    isUsed = used;
 	  }
 
 	  //Alternate constructor
-	  public Car(String model, String make) {
+	  public Car(String make, String model) {
 	    //This constructor only uses the name and breed
 	    //Set the year and price to random #s
-
+		String[] colours = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple", "White", "Black", "Brown", "Electric Turquoise"};
 	    carModel = model;
 	    carMake = make;
-	    carYear = (int) (Math.random() * 37) + 1980;
-	    carPrice = "$"+(((int) (Math.random() * 999))*100) ;
+	    carColour = colours[(int) (10*Math.random())];
+	    carYear = (int) (Math.random() * 67) + 1950;
+	    carPrice = (int) (Math.random() * 999)*100;
+	    isUsed = ThreadLocalRandom.current().nextBoolean();
 	  }
 
 	  public void barkFriendly() {
-	    System.out.println("Arf! Arf!");
+	    System.out.println("Vroom! Vroom!");
 	  }
 
 	  public void barkAngry() {
-	    System.out.println("GRR! RRRFFF!");
+	    System.out.println("SKRRT! SKRRRRRRT!");
 	  }
 
+	  public void honk() {
+	    System.out.println("HONK");
+	  }
+	  
 	  //method to display all info of the Car
 	  public String toString() {
-	    String output = "Model: " + carModel + "\n";
-	    output += "Make: " + carMake + "\n";
-	    output += "Year: " + carYear + "\n";
-	    output += "Price: " + carPrice;
+		DecimalFormat format = new DecimalFormat("0.00");
+	    String output = "Make:		" + carMake + "\n";
+	    output += "Model:		" + carModel + "\n";
+	    output += "Colour:		" + carColour+"\n";
+	    output += "Price:		$" + format.format(carPrice)+"\n";
+	    if(carYear<0){
+		    output += "Year:		" + -carYear + " BCE\n";
+	    } else {
+	    output += "Year:		" + carYear + "\n";
+	    }
+	    output += "Second-hand:	";
+	    if(isUsed){
+	    	output+= "Yes";
+	    } else {
+	    	output+= "No";
+	    }
 	    //output string is complete, return it
 	    return output;
 	  }
--- McAseySeanU3A1/src/a1/CarTester.java
+++ McAseySeanU3A1/src/a1/CarTester.java
@@ -1,26 +1,79 @@
 package a1;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class CarTester {
 
 	public static void main(String[] args) {
-	    Car Car1 = new Car("Rover", "Spaniel", 8, 4);
-	    Car Car2 = new Car("Spot", "Border Collie");
-	    Car Car3 = new Car();
+		Scanner a = new Scanner(System.in);
+		boolean done = false, isUsed = false, flag = false;
+		String make1 = "", model1 = "", make = "", model = "", colour = "";
+		int price = 0, year = 0;
+		System.out.println("Please enter the make of the first car:");
+		make = a.nextLine();
+		System.out.println("Please enter the model of the first car:");
+		model = a.nextLine();
+		System.out.println("Please enter the colour of the first car:");
+		colour = a.nextLine();
+		System.out.println("Please enter the year of the first car:");
+		do {
+			try {
+				year = Integer.parseInt(a.nextLine());
+				flag = true;
+			} catch (NumberFormatException error) {
+				System.out.println("This input was not a valid integer. Please try again.");
+				flag = false;
+			}
+		} while (!flag);
+		System.out.println("Please enter the price of the first car:");
+		do {
+			try {
+				price = Integer.parseInt(a.nextLine());
+				flag = true;
+			} catch (NumberFormatException error) {
+				System.out.println("This input was not a valid integer. Please try again.");
+				flag = false;
+			}
+		} while (!flag);
+		System.out.println("Is the first car used? Y/N");
+		do {
+			String i = a.nextLine().trim();
+			if (i.equalsIgnoreCase("y") || i.equalsIgnoreCase("yes") || i.equalsIgnoreCase("true")) {
+				isUsed = true;
+				flag = true;
+			} else if (i.equalsIgnoreCase("n") || i.equalsIgnoreCase("false") || i.equalsIgnoreCase("no")) {
+				isUsed = false;
+				flag = true;
+			} else {
+				System.out.println(
+						"This input was not a valid response. Please try again with the letter Y (yes) or N (no).");
+			}
+		} while (!flag);
 
-	    //Print the details of each Car
-	    System.out.println(Car1.toString());
-	    System.out.println("--------------------------");
+		System.out.println("Please enter the make of the second car:");
+		make1 = a.nextLine();
+		System.out.println("Please enter the model of the second car:");
+		model1 = a.nextLine();
 
-	    System.out.println(Car2.toString());
-	    System.out.println("--------------------------");
+		Car Car1 = new Car(make, model, colour, year, price, isUsed);
+		Car Car2 = new Car(make1, model1);
+		Car Car3 = new Car();
 
-	    //toString() is called by default, no need to write it!
-	    System.out.println(Car3);
-	    System.out.println("--------------------------");
+		// Print the details of each Car
+		System.out.println(Car1.toString());
+		System.out.println("--------------------------");
 
-	    //Car1 barks friendly, Car2 barks angry
-	    Car1.barkFriendly();
-	    Car2.barkAngry();
-	  }
+		System.out.println(Car2.toString());
+		System.out.println("--------------------------");
+
+		// toString() is called by default, no need to write it!
+		System.out.println(Car3);
+		System.out.println("--------------------------");
+
+		// Car1 barks friendly, Car2 barks angry
+		Car1.barkFriendly();
+		Car2.barkAngry();
+	}
 
 }
